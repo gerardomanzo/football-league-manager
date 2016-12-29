91472528e9c553c123744ef9fcfb2bf7dd23f815
giovannicap94 <capaldogiovanni@hotmail.it>
2016-12-29 16:16:08 +0100

    Aggiunta classe Informazioni

---
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/partite/Informazioni.java
@@ -0,0 +1,99 @@
+package flm.partite;
+
+public class Informazioni {
+	private int goal;
+	private int assist;
+	private int cartellino;
+	private int squalifica;
+	private String motivazione;
+	
+	static final int NESSUN_CARTELLINO = 0;
+	static final int GIALLO = 1;
+	static final int DOPPIO_GIALLO = 2;
+	static final int ROSSO = 3;
+	
+	/**
+	 * @param goal
+	 * @param assist
+	 * @param cartellino
+	 * @param squalifica
+	 * @param motivazione
+	 */
+	public Informazioni() {
+		this.goal = 0;
+		this.assist = 0;
+		this.cartellino = NESSUN_CARTELLINO;
+		this.squalifica = 0;
+		this.motivazione = "";
+	}
+
+	/**
+	 * @return the goal
+	 */
+	public int getGoal() {
+		return goal;
+	}
+
+	/**
+	 * @param goal the goal to set
+	 */
+	public void setGoal(int goal) {
+		this.goal = goal;
+	}
+
+	/**
+	 * @return the assist
+	 */
+	public int getAssist() {
+		return assist;
+	}
+
+	/**
+	 * @param assist the assist to set
+	 */
+	public void setAssist(int assist) {
+		this.assist = assist;
+	}
+
+	/**
+	 * @return the cartellino
+	 */
+	public int getCartellino() {
+		return cartellino;
+	}
+
+	/**
+	 * @param cartellino the cartellino to set
+	 */
+	public void setCartellino(int cartellino) {
+		this.cartellino = cartellino;
+	}
+
+	/**
+	 * @return the squalifica
+	 */
+	public int getSqualifica() {
+		return squalifica;
+	}
+
+	/**
+	 * @param squalifica the squalifica to set
+	 */
+	public void setSqualifica(int squalifica) {
+		this.squalifica = squalifica;
+	}
+
+	/**
+	 * @return the motivazione
+	 */
+	public String getMotivazione() {
+		return motivazione;
+	}
+
+	/**
+	 * @param motivazione the motivazione to set
+	 */
+	public void setMotivazione(String motivazione) {
+		this.motivazione = motivazione;
+	}
+}
