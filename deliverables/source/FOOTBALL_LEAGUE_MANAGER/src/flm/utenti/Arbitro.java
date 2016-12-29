10e2b6fdb16368a06f31789b350d9465162ade35
christiangiuseppe <nocerino.christian.cn@gmail.com>
2016-12-29 18:06:30 +0100

    Aggiunta la classe Arbitro

---
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/utenti/Arbitro.java
@@ -0,0 +1,77 @@
+package flm.utenti;
+
+public class Arbitro {
+	private String nome;
+	private String cognome;
+	private String username;
+	private String password;
+	
+	/**
+	 * @param nome
+	 * @param cognome
+	 * @param username
+	 * @param password
+	 */
+	public Arbitro(String nome, String cognome, String username, String password) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
+	}
+	
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	/**
+	 * @return the cognome
+	 */
+	public String getCognome() {
+		return cognome;
+	}
+	
+	/**
+	 * @param cognome the cognome to set
+	 */
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
