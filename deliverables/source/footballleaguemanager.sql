b963f9b37fbd8b4143d26efe916fe8a0d4b58dfe
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-03 19:08:20 +0100

    Update footballleaguemanager.sql

--- deliverables/source/footballleaguemanager.sql
+++ deliverables/source/footballleaguemanager.sql
@@ -10,7 +10,8 @@ SET time_zone = "+00:00";
 
 --
 -- Database: `footballleaguemanager`
-   CREATE DATABASE footballleaguemanager;
+   
+CREATE DATABASE footballleaguemanager;
 --
 
 -- --------------------------------------------------------
@@ -53,13 +54,7 @@ CREATE TABLE IF NOT EXISTS `Informazioni` (
   `Assist` int(5) NOT NULL,
   `Cartellino` varchar(15) NOT NULL,
   `Motivazione` varchar(30) NOT NULL,
-  `Squalifica` int(5) NOT NULL,
-  KEY `ID_Giocatore` (`ID_Giocatore`),
-  KEY `ID_Partita` (`ID_Partita`),
-  KEY `ID_Partita_2` (`ID_Partita`),
-  KEY `ID_Partita_3` (`ID_Partita`),
-  KEY `ID_Partita_4` (`ID_Partita`),
-  KEY `ID_Partita_5` (`ID_Partita`)
+  `Squalifica` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -70,9 +65,7 @@ CREATE TABLE IF NOT EXISTS `Informazioni` (
 
 CREATE TABLE IF NOT EXISTS `Partecipazione` (
   `ID_Squadra` int(5) NOT NULL,
-  `ID_Giocatore` int(5) NOT NULL,
-  KEY `ID_Squadra` (`ID_Squadra`),
-  KEY `ID_Giocatore` (`ID_Giocatore`)
+  `ID_Giocatore` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -91,13 +84,7 @@ CREATE TABLE IF NOT EXISTS `Partita` (
   `GoalCasa` int(5) NOT NULL,
   `GoalOspite` int(5) NOT NULL,
   `ID_Arbitro` int(5) NOT NULL,
-  PRIMARY KEY (`ID_Partita`),
-  KEY `ID_Campionato` (`ID_Campionato`),
-  KEY `ID_Arbitro` (`ID_Arbitro`),
-  KEY `ID_Casa` (`ID_Casa`),
-  KEY `ID_Casa_2` (`ID_Casa`),
-  KEY `ID_Casa_3` (`ID_Casa`),
-  KEY `ID_Ospite` (`ID_Ospite`)
+  PRIMARY KEY (`ID_Partita`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -117,9 +104,7 @@ CREATE TABLE IF NOT EXISTS `Squadra` (
   `GoalFatti` int(5) NOT NULL,
   `GoalSubiti` int(5) NOT NULL,
   `StatoIscrizione` tinyint(1) NOT NULL,
-  PRIMARY KEY (`ID_Squadra`),
-  KEY `ID_Allenatore` (`ID_Allenatore`),
-  KEY `ID_Campionato` (`ID_Campionato`)
+  PRIMARY KEY (`ID_Squadra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -139,36 +124,33 @@ CREATE TABLE IF NOT EXISTS `Utenti` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `Informazioni`
+-- Vincoli per la tabella `Informazioni`
 --
 ALTER TABLE `Informazioni`
   ADD CONSTRAINT `Informazioni_ibfk_1` FOREIGN KEY (`ID_Partita`) REFERENCES `Partita` (`ID_Partita`),
   ADD CONSTRAINT `Informazioni_ibfk_2` FOREIGN KEY (`ID_Giocatore`) REFERENCES `Giocatore` (`ID_Giocatore`);
 
 --
--- Limiti per la tabella `Partecipazione`
+-- Vincoli per la tabella `Partecipazione`
 --
 ALTER TABLE `Partecipazione`
   ADD CONSTRAINT `Partecipazione_ibfk_1` FOREIGN KEY (`ID_Squadra`) REFERENCES `Squadra` (`ID_Squadra`),
   ADD CONSTRAINT `Partecipazione_ibfk_2` FOREIGN KEY (`ID_Giocatore`) REFERENCES `Giocatore` (`ID_Giocatore`);
 
 --
--- Limiti per la tabella `Partita`
+-- Vincoli per la tabella `Partita`
 --
 ALTER TABLE `Partita`
   ADD CONSTRAINT `Partita_ibfk_1` FOREIGN KEY (`ID_Campionato`) REFERENCES `Campionato` (`ID_Campionato`),
-  ADD CONSTRAINT `Partita_ibfk_2` FOREIGN KEY (`ID_Arbitro`) REFERENCES `Utenti` (`ID_Arbitro`),
+  ADD CONSTRAINT `Partita_ibfk_2` FOREIGN KEY (`ID_Arbitro`) REFERENCES `Utenti` (`ID_Utente`),
   ADD CONSTRAINT `Partita_ibfk_3` FOREIGN KEY (`ID_Casa`) REFERENCES `Squadra` (`ID_Squadra`),
   ADD CONSTRAINT `Partita_ibfk_4` FOREIGN KEY (`ID_Ospite`) REFERENCES `Squadra` (`ID_Squadra`);
 
 --
--- Limiti per la tabella `Squadra`
+-- Vincoli per la tabella `Squadra`
 --
 ALTER TABLE `Squadra`
+  ADD CONSTRAINT `Squadra_ibfk_1` FOREIGN KEY (`ID_Allenatore`) REFERENCES `Utenti` (`ID_Utente`),
   ADD CONSTRAINT `Squadra_ibfk_2` FOREIGN KEY (`ID_Campionato`) REFERENCES `Campionato` (`ID_Campionato`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
