6549522397442c092be43853e6a5e5caafe032b8
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-03 15:14:51 +0100

    Pulizia codice

--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/utenti/Allenatore.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/utenti/Allenatore.java
@@ -1,13 +1,6 @@
 package flm.utenti;
 
 public class Allenatore extends Utente {
-
-	/**
-	 * @param nome
-	 * @param cognome
-	 * @param email
-	 * @param password
-	 */
 	public Allenatore() {
 		super();
 	}
--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/utenti/Arbitro.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/utenti/Arbitro.java
@@ -1,13 +1,6 @@
 package flm.utenti;
 
 public class Arbitro extends Utente {
-
-	/**
-	 * @param nome
-	 * @param cognome
-	 * @param email
-	 * @param password
-	 */
 	public Arbitro() {
 		super();
 	}
