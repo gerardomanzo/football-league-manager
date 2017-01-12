7d639a1159f52046a17cc9f6ca707b2be1d63077
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-12 15:45:54 +0100

    Modificato metodo 'modificaGiocatore' di GiocatoreModel e GiocatoreControl

--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/giocatori/GiocatoreControl.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/giocatori/GiocatoreControl.java
@@ -1,6 +1,7 @@
 package flm.giocatori;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,13 +15,37 @@ import flm.utenti.Utente;
 public class GiocatoreControl extends HttpServlet{
 	private static final long serialVersionUID = -8462132438315054976L;
 	private static GiocatoreManager model = new GiocatoreManager();
-	
+
 	public GiocatoreControl() {
 		super();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+		try {
+			HttpSession session = request.getSession();
+			Utente utente = (Utente) session.getAttribute("utente");
+
+			String ruolo = (String) session.getAttribute("ruolo");
+
+			String action = request.getParameter("action");
+
+			if(action != null) {
+				if(action.equalsIgnoreCase("getGiocatori")) {
+					if(utente != null && ruolo.equalsIgnoreCase("amministratore")) {
+						Collection<Giocatore> giocatori = model.leggiGiocatori();
+
+						request.removeAttribute("giocatori");
+						request.setAttribute("giocatori", giocatori);
+
+						RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/modificaGiocatore.jsp");
+						dispatcher.forward(request, response);
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Error:" + e.getMessage());
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -35,18 +60,16 @@ public class GiocatoreControl extends HttpServlet{
 			if(action != null) {
 				if(action.equalsIgnoreCase("modificaGiocatore")) {
 					if(utente != null && ruolo.equalsIgnoreCase("amministratore")){
-						String nome =  request.getParameter("nomeGiocatore");
-						String cognome = request.getParameter("cognomeGiocatore");
+						int id =  Integer.parseInt(request.getParameter("giocatore"));
 						String nomeNuovo =  request.getParameter("nuovoNome");
 						String cognomeNuovo = request.getParameter("nuovoCognome");
 						Giocatore giocatore = new Giocatore();
-						giocatore.setNome(nome);
-						giocatore.setCognome(cognome);
+						giocatore.setID(id);
 						model.modificaGiocatore(giocatore, nomeNuovo, cognomeNuovo);
-	
+
 						RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/areaAmministratore.jsp");
 						dispatcher.forward(request, response);
-	
+
 					}
 				}
 				else if(action.equalsIgnoreCase("iscrizioneGiocatore")) {
@@ -57,7 +80,7 @@ public class GiocatoreControl extends HttpServlet{
 						giocatore.setNome(nome);
 						giocatore.setCognome(cognome);
 						model.creaGiocatore(giocatore);
-	
+
 						RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/areaAmministratore.jsp");
 						dispatcher.forward(request, response);
 					}
--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/giocatori/GiocatoreManager.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/giocatori/GiocatoreManager.java
@@ -2,56 +2,99 @@ package flm.giocatori;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import flm.storage.DriverManagerConnectionPool;
 
 public class GiocatoreManager {
-	private static final String TABLE_GIOCATORI = "Giocatore";	
+	private static final String TABLE_GIOCATORI = "Giocatore";
+
 	public void modificaGiocatore(Giocatore giocatore,String nome,String cognome) throws SQLException{
-		Connection connection=null;
+		Connection connection = null;
 		PreparedStatement preparedStatement = null;
-		String updateSQL = "UPDATE "+ GiocatoreManager.TABLE_GIOCATORI + " SET Nome=?, Cognome=? WHERE Nome=? AND Cognome=?";
+		String updateSQL = "UPDATE "+ GiocatoreManager.TABLE_GIOCATORI + " SET Nome=?, Cognome=? WHERE ID_Giocatore=?";
 		try{
 			connection = DriverManagerConnectionPool.getConnection();
 			preparedStatement = connection.prepareStatement(updateSQL);
-		    preparedStatement.setString(1, nome);
-		    preparedStatement.setString(2, cognome);
-		    preparedStatement.setString(3, giocatore.getNome());    
-		    preparedStatement.setString(4, giocatore.getCognome());
-		    preparedStatement.executeUpdate();
-		    connection.commit();
-		}finally{
+			preparedStatement.setString(1, nome);
+			preparedStatement.setString(2, cognome);
+			preparedStatement.setInt(3, giocatore.getID());
+			preparedStatement.executeUpdate();
+			connection.commit();
+		}
+		finally{
 			try {
 				if (preparedStatement != null) {
 					preparedStatement.close();
 				}
-			} finally {
-			 DriverManagerConnectionPool.releaseConnection(connection);
+			}
+			finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
 			}
 		}
 	}
-	
+
 	public void creaGiocatore(Giocatore giocatore) throws SQLException {
-		Connection connection=null;
+		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String insertSQL = "INSERT INTO "+ GiocatoreManager.TABLE_GIOCATORI + "(Nome, Cognome) VALUES(?, ?)";
 		try{
 			connection = DriverManagerConnectionPool.getConnection();
 			preparedStatement = connection.prepareStatement(insertSQL);
-		    preparedStatement.setString(1, giocatore.getNome());    
-		    preparedStatement.setString(2, giocatore.getCognome());
-		    
-		    preparedStatement.executeUpdate();
-		    connection.commit();
-		}finally{
+			preparedStatement.setString(1, giocatore.getNome());    
+			preparedStatement.setString(2, giocatore.getCognome());
+
+			preparedStatement.executeUpdate();
+			connection.commit();
+		}
+		finally{
 			try {
 				if (preparedStatement != null) {
 					preparedStatement.close();
 				}
-			} finally {
-			 DriverManagerConnectionPool.releaseConnection(connection);
+			}
+			finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
 			}
 		}
+	}
+
+	public Collection<Giocatore> leggiGiocatori() throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		String selectSQL = "SELECT * FROM "+ GiocatoreManager.TABLE_GIOCATORI;
+
+		Collection<Giocatore> lista = new LinkedList<Giocatore>();
+
+		try{
+			connection = DriverManagerConnectionPool.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while(rs.next()) {
+				Giocatore giocatore = new Giocatore();
+
+				giocatore.setID(rs.getInt("ID_Giocatore"));
+				giocatore.setNome(rs.getString("Nome"));
+				giocatore.setCognome(rs.getString("Cognome"));
+
+				lista.add(giocatore);
+			}
+		}
+		finally{
+			try {
+				if (preparedStatement != null) {
+					preparedStatement.close();
+				}
+			}
+			finally {
+				DriverManagerConnectionPool.releaseConnection(connection);
+			}
+		}
+
+		return lista;
 	}
 }
