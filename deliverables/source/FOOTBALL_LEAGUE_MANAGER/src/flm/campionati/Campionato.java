4decfee6454aa590f28d102a9a128ae44b345d6a
Raffox24 <raf24x@hotmail.it>
2017-01-20 16:27:19 +0100

    Aggiunto visualizza Calendario

--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/campionati/Campionato.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/campionati/Campionato.java
@@ -1,8 +1,8 @@
 package flm.campionati;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -97,69 +97,15 @@ public class Campionato {
 
 		squadre.put(squadra.getNomeSquadra(), squadra);
 		squadra.iscriviSquadra();
-
-		if(squadre.size() == numSquadre)
-			generaCalendario();
 	}
 
-	private void generaCalendario() {
-		int giornate = numSquadre-1;
-		Iterator<Squadra> iteratorSquadre = squadre.values().iterator();
-		Squadra[] casa = new Squadra[numSquadre/2];
-		Squadra[] ospite = new Squadra[numSquadre/2];
-
-		for(int i = 0; i < numSquadre/2; i++) {
-			casa[i] = iteratorSquadre.next();
-			ospite[i] = iteratorSquadre.next(); 
-		}
-
-		for(int i = 0; i < giornate; i++) {
-			for(int j = 0; j < numSquadre /2 ; j++) {
-				Partita partita = new Partita();
-				partita.setGiornata(i+1);
-
-				if(j % 2 == 0) {
-					partita.setCasa(casa[j]);
-					partita.setOspite(ospite[j]);
-				}
-				else {
-					partita.setCasa(ospite[j]);
-					partita.setOspite(casa[j]);
-				}
-
-				calendario.add(partita);
-			}
-
-			// salva l'elemento fisso
-			Squadra pivot = casa[0];
-
-			// sposta in avanti gli elementi di "ospite" inserendo 
-			// all'inizio l'elemento casa[1] e salva l'elemento uscente in "riporto"
-			Squadra riporto = ospite[ospite.length - 1];
-			ospite = shiftRight(ospite, casa[1]);
-
-			// sposta a sinistra gli elementi di "casa" inserendo all'ultimo 
-			// posto l'elemento "riporto" */
-			casa = shiftLeft(casa, riporto);
-
-			// ripristina l'elemento fisso
-			casa[0] = pivot ;
-		}
+	public Collection<Squadra> getSquadre()
+	{
+		return squadre.values();
 	}
 
-	private Squadra[] shiftLeft(Squadra[] data, Squadra add) {
-		Squadra[] temp = new Squadra[data.length];
-		for(int i = 0; i < data.length-1; i++)
-			temp[i] = data[i + 1];
-		temp[data.length - 1] = add;
-		return temp;
-	}
-
-	private Squadra[] shiftRight(Squadra[] data, Squadra add) {
-		Squadra[] temp = new Squadra[data.length];
-		for (int i = 1; i < data.length; i++)
-			temp[i] = data[i - 1];
-		temp[0] = add;
-		return temp;
+	public void aggiungiPartita (Partita partita)
+	{
+		calendario.add(partita);
 	}
 }
