42467e5237229b45bc369a0aa369a5eef5ab4468
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-21 12:16:50 +0100

    Pulizia codice

--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/squadre/SquadraControl.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/squadre/SquadraControl.java
@@ -14,6 +14,7 @@ import flm.giocatori.Giocatore;
 import flm.giocatori.GiocatoreManager;
 import flm.utenti.Allenatore;
 import flm.utenti.Utente;
+
 public class SquadraControl extends HttpServlet{
 	private static final long serialVersionUID = -2530651357994741963L;
 	private static SquadreManager modelSquadre = new SquadreManager();
