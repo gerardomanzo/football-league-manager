8fac06383c36f8c3638d11fadb8b7a63aed4b7ad
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-21 19:24:59 +0100

    Aggiornate colonne tabelle

--- deliverables/source/footballleaguemanager.sql
+++ deliverables/source/footballleaguemanager.sql
@@ -1,99 +1,129 @@
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+CREATE DATABASE  IF NOT EXISTS `footballleaguemanager` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `footballleaguemanager`;
+-- MySQL dump 10.13  Distrib 5.7.13, for linux-glibc2.5 (x86_64)
+--
+-- Host: 127.0.0.1    Database: footballleaguemanager
+-- ------------------------------------------------------
+-- Server version	5.7.16-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `footballleaguemanager`
-   
-CREATE DATABASE footballleaguemanager;
+-- Table structure for table `Campionato`
 --
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `Campionato`
---
-
-CREATE TABLE IF NOT EXISTS `Campionato` (
+DROP TABLE IF EXISTS `Campionato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Campionato` (
   `ID_Campionato` int(5) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(25) NOT NULL,
   `NumSquadre` int(5) NOT NULL,
   `Quota` float NOT NULL,
   PRIMARY KEY (`ID_Campionato`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `Giocatore`
+-- Table structure for table `Giocatore`
 --
 
-CREATE TABLE IF NOT EXISTS `Giocatore` (
+DROP TABLE IF EXISTS `Giocatore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Giocatore` (
   `ID_Giocatore` int(5) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(25) NOT NULL,
   `Cognome` varchar(25) NOT NULL,
   PRIMARY KEY (`ID_Giocatore`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `Informazioni`
+-- Table structure for table `Informazioni`
 --
 
-CREATE TABLE IF NOT EXISTS `Informazioni` (
+DROP TABLE IF EXISTS `Informazioni`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Informazioni` (
   `ID_Partita` int(5) NOT NULL,
   `ID_Giocatore` int(5) NOT NULL,
   `Goal` smallint(6) NOT NULL,
   `Assist` int(5) NOT NULL,
-  `Cartellino` varchar(15) NOT NULL,
+  `Cartellino` int(5) NOT NULL,
   `Motivazione` varchar(30) NOT NULL,
-  `Squalifica` int(5) NOT NULL
+  `Squalifica` int(5) NOT NULL,
+  KEY `Informazioni_ibfk_1` (`ID_Partita`),
+  KEY `Informazioni_ibfk_2` (`ID_Giocatore`),
+  CONSTRAINT `Informazioni_ibfk_1` FOREIGN KEY (`ID_Partita`) REFERENCES `Partita` (`ID_Partita`),
+  CONSTRAINT `Informazioni_ibfk_2` FOREIGN KEY (`ID_Giocatore`) REFERENCES `Giocatore` (`ID_Giocatore`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `Partecipazione`
+-- Table structure for table `Partecipazione`
 --
 
-CREATE TABLE IF NOT EXISTS `Partecipazione` (
+DROP TABLE IF EXISTS `Partecipazione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Partecipazione` (
   `ID_Squadra` int(5) NOT NULL,
-  `ID_Giocatore` int(5) NOT NULL
+  `ID_Giocatore` int(5) NOT NULL,
+  KEY `Partecipazione_ibfk_1` (`ID_Squadra`),
+  KEY `Partecipazione_ibfk_2` (`ID_Giocatore`),
+  CONSTRAINT `Partecipazione_ibfk_1` FOREIGN KEY (`ID_Squadra`) REFERENCES `Squadra` (`ID_Squadra`),
+  CONSTRAINT `Partecipazione_ibfk_2` FOREIGN KEY (`ID_Giocatore`) REFERENCES `Giocatore` (`ID_Giocatore`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `Partita`
+-- Table structure for table `Partita`
 --
 
-CREATE TABLE IF NOT EXISTS `Partita` (
+DROP TABLE IF EXISTS `Partita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Partita` (
   `ID_Partita` int(5) NOT NULL AUTO_INCREMENT,
   `ID_Campionato` int(5) NOT NULL,
   `ID_Casa` int(5) NOT NULL,
   `ID_Ospite` int(5) NOT NULL,
   `Giornata` int(5) NOT NULL,
-  `Data` date NOT NULL,
-  `GoalCasa` int(5) NOT NULL,
-  `GoalOspite` int(5) NOT NULL,
-  `ID_Arbitro` int(5) NOT NULL,
-  PRIMARY KEY (`ID_Partita`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  `Data` date DEFAULT NULL,
+  `GoalCasa` int(5) DEFAULT NULL,
+  `GoalOspite` int(5) DEFAULT NULL,
+  `ID_Arbitro` int(5) DEFAULT NULL,
+  PRIMARY KEY (`ID_Partita`),
+  KEY `Partita_ibfk_1` (`ID_Campionato`),
+  KEY `Partita_ibfk_3` (`ID_Casa`),
+  KEY `Partita_ibfk_4` (`ID_Ospite`),
+  KEY `Partita_ibfk_2` (`ID_Arbitro`),
+  CONSTRAINT `Partita_ibfk_1` FOREIGN KEY (`ID_Campionato`) REFERENCES `Campionato` (`ID_Campionato`),
+  CONSTRAINT `Partita_ibfk_2` FOREIGN KEY (`ID_Arbitro`) REFERENCES `Utente` (`ID_Utente`),
+  CONSTRAINT `Partita_ibfk_3` FOREIGN KEY (`ID_Casa`) REFERENCES `Squadra` (`ID_Squadra`),
+  CONSTRAINT `Partita_ibfk_4` FOREIGN KEY (`ID_Ospite`) REFERENCES `Squadra` (`ID_Squadra`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `Squadra`
+-- Table structure for table `Squadra`
 --
 
-CREATE TABLE IF NOT EXISTS `Squadra` (
+DROP TABLE IF EXISTS `Squadra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Squadra` (
   `ID_Squadra` int(5) NOT NULL AUTO_INCREMENT,
   `ID_Allenatore` int(5) NOT NULL,
   `ID_Campionato` int(5) DEFAULT NULL,
@@ -104,16 +134,22 @@ CREATE TABLE IF NOT EXISTS `Squadra` (
   `GoalFatti` int(5) DEFAULT '0',
   `GoalSubiti` int(5) DEFAULT '0',
   `StatoIscrizione` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`ID_Squadra`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  PRIMARY KEY (`ID_Squadra`),
+  KEY `Squadra_ibfk_1` (`ID_Allenatore`),
+  KEY `Squadra_ibfk_2` (`ID_Campionato`),
+  CONSTRAINT `Squadra_ibfk_1` FOREIGN KEY (`ID_Allenatore`) REFERENCES `Utente` (`ID_Utente`),
+  CONSTRAINT `Squadra_ibfk_2` FOREIGN KEY (`ID_Campionato`) REFERENCES `Campionato` (`ID_Campionato`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `Utente`
+-- Table structure for table `Utente`
 --
 
-CREATE TABLE IF NOT EXISTS `Utente` (
+DROP TABLE IF EXISTS `Utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Utente` (
   `ID_Utente` int(5) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(25) NOT NULL,
   `Cognome` varchar(25) NOT NULL,
@@ -121,38 +157,16 @@ CREATE TABLE IF NOT EXISTS `Utente` (
   `Password` varchar(25) NOT NULL,
   `Ruolo` char(1) NOT NULL,
   PRIMARY KEY (`ID_Utente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Vincoli per la tabella `Informazioni`
---
-ALTER TABLE `Informazioni`
-  ADD CONSTRAINT `Informazioni_ibfk_1` FOREIGN KEY (`ID_Partita`) REFERENCES `Partita` (`ID_Partita`),
-  ADD CONSTRAINT `Informazioni_ibfk_2` FOREIGN KEY (`ID_Giocatore`) REFERENCES `Giocatore` (`ID_Giocatore`);
-
---
--- Vincoli per la tabella `Partecipazione`
---
-ALTER TABLE `Partecipazione`
-  ADD CONSTRAINT `Partecipazione_ibfk_1` FOREIGN KEY (`ID_Squadra`) REFERENCES `Squadra` (`ID_Squadra`),
-  ADD CONSTRAINT `Partecipazione_ibfk_2` FOREIGN KEY (`ID_Giocatore`) REFERENCES `Giocatore` (`ID_Giocatore`);
-
---
--- Vincoli per la tabella `Partita`
---
-ALTER TABLE `Partita`
-  ADD CONSTRAINT `Partita_ibfk_1` FOREIGN KEY (`ID_Campionato`) REFERENCES `Campionato` (`ID_Campionato`),
-  ADD CONSTRAINT `Partita_ibfk_2` FOREIGN KEY (`ID_Arbitro`) REFERENCES `Utente` (`ID_Utente`),
-  ADD CONSTRAINT `Partita_ibfk_3` FOREIGN KEY (`ID_Casa`) REFERENCES `Squadra` (`ID_Squadra`),
-  ADD CONSTRAINT `Partita_ibfk_4` FOREIGN KEY (`ID_Ospite`) REFERENCES `Squadra` (`ID_Squadra`);
-
---
--- Vincoli per la tabella `Squadra`
---
-ALTER TABLE `Squadra`
-  ADD CONSTRAINT `Squadra_ibfk_1` FOREIGN KEY (`ID_Allenatore`) REFERENCES `Utente` (`ID_Utente`),
-  ADD CONSTRAINT `Squadra_ibfk_2` FOREIGN KEY (`ID_Campionato`) REFERENCES `Campionato` (`ID_Campionato`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-21 19:23:15
