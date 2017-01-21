4c9ef8348c8a41c523e995903f3159f0df3bfb8f
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-21 12:14:11 +0100

    Aggiunta assegnazione arbitro alla classe PartitaControl

--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/partite/PartitaControl.java
+++ deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/partite/PartitaControl.java
@@ -15,7 +15,7 @@ import flm.utenti.Utente;
 import flm.utenti.UtentiManager;
 
 public class PartitaControl extends HttpServlet{
-	
+
 	/**
 	 * 
 	 */
@@ -26,29 +26,29 @@ public class PartitaControl extends HttpServlet{
 	public PartitaControl() {
 		super();
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
-		
+
 		try {
 			if(action != null) {
 				if(action.equalsIgnoreCase("assegnaArbitro")) {
 					HttpSession session = request.getSession();
 					Utente utente = (Utente) session.getAttribute("utente");
-														
+
 					String ruolo = (String) session.getAttribute("ruolo");
-					
+
 					if(utente != null && ruolo.equalsIgnoreCase("amministratore")) {
 						Collection<Arbitro> arbitri = modelUtenti.leggiArbitri();
 						Collection<Partita> partite = modelPartite.cercaPartite();
-						
+
 						request.removeAttribute("arbitri");
 						request.setAttribute("arbitri", arbitri);
-						
+
 						request.removeAttribute("partite");
 						request.setAttribute("partite", partite);
-						
+
 						RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/assegnazioneArbitro.jsp");
 						dispatcher.forward(request, response);
 					}
@@ -62,6 +62,38 @@ public class PartitaControl extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		String action = request.getParameter("action");
+
+		try {
+			if(action != null) {
+				if(action.equalsIgnoreCase("assegnaArbitro")) {
+					HttpSession session = request.getSession();
+					Utente utente = (Utente) session.getAttribute("utente");
+
+					String ruolo = (String) session.getAttribute("ruolo");
+
+					if(utente != null && ruolo.equalsIgnoreCase("amministratore")) {
+						
+						int id_arbitro = Integer.parseInt(request.getParameter("arbitro"));
+						int id_partita = Integer.parseInt(request.getParameter("partita"));
+						
+						Arbitro arbitro = new Arbitro();
+						arbitro.setID(id_arbitro);
+						
+						Partita partita = new Partita();
+						partita.setID(id_partita);
+						partita.setArbitro(arbitro);
+						
+						modelPartite.assegnaArbitro(arbitro, partita);
+						
+						RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/areaAmministratore.jsp");
+						dispatcher.forward(request, response);
+					}
+				}
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Error:" + e.getMessage());
+		}
 	}	
 }
