eba89f5cb4181194795f0cc6c8844b621354bbde
gerardomanzo <gerardo.manzo@outlook.com>
2017-01-26 12:50:01 +0100

    Rimosse classi eccezioni

--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/campionati/SquadraGiaIscrittaException.java
+++
@@ -1,5 +0,0 @@
-package flm.campionati;
-
-public class SquadraGiaIscrittaException extends Exception {
-	private static final long serialVersionUID = 2837611481213581308L;
-}
--- deliverables/source/FOOTBALL_LEAGUE_MANAGER/src/flm/squadre/GiocatoreGiaPresenteException.java
+++
@@ -1,5 +0,0 @@
-package flm.squadre;
-
-public class GiocatoreGiaPresenteException extends Exception {
-	private static final long serialVersionUID = 3921658018270269374L;
-}
